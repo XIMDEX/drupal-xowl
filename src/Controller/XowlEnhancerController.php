27c7b0a2b66582f4bc43270407823302515c2c8c
DrZippie <antonio@antoniocortes.com>
2015-02-03 11:08:11 +0100

    maked a fake stanbol to test

--- src/Controller/XowlEnhancerController.php
+++ src/Controller/XowlEnhancerController.php
@@ -9,74 +9,53 @@ namespace Drupal\xowl\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 use Drupal\Core\Url;
- 
+
 /**
  * Controller routines for block example routes.
  */
 class XowlEnhancerController  extends ControllerBase {
-  /**
-   * A simple page to explain to the developer what to do.
-   */
-  public function enhance() {
-   
-    $config = \Drupal::config('xowl.settings');
-    $xowlEndpoint  = $config->get('endpoint');
-     $service = new \Drupal\xowl\Service\StanbolService( $xowlEndpoint ) ;
-    $content = filter_input(INPUT_POST, 'content');
+    /**
+     * A simple page to explain to the developer what to do.
+     */
+    public function enhance() {
+
+        $config = \Drupal::config('xowl.settings');
+        $xowlEndpoint  = $config->get('endpoint');
+        $content = filter_input(INPUT_POST, 'content');
 
 
-    $cleaner = new \Drupal\xowl\Service\StanbolCleaner( $content , ' ' ) ;
-    $cleaned = $cleaner->clean();
-    $stanbolResponse  = $service->suggest( $cleaned  )->getData() ;
 
 
-    foreach( $stanbolResponse['semantic'] as $key => $values ) {
+        $client = new \GuzzleHttp\Client();
 
-      $stanbolResponse['semantic'][ $key ]['start'] = $cleaner->getNewOffset( $values['start']);
-      $stanbolResponse['semantic'][ $key ]['end'] =   $cleaner->getNewOffset( $values['end'] ,$values['end'] -  $values['start']   );
+        $headers = array( );
 
+        $data = array(
+            'token' =>  '000-00000-000' ,
+            'content' => $content
+
+        );
+
+
+        $response = $client->post( $xowlEndpoint . 'v1/enhance', [
+            'headers' => $headers,
+            'allow_redirects' => true,
+            'body' => $data,
+            'timeout' => 30
+        ]);
+
+        // Expected result.
+        $data = json_decode( (string)$response->getBody() ,true  );
+
+
+
+        header('Content-type: application/json');
+        die( json_encode( $data  ) ) ;
 
     }
-
-
-  $text = $content ;
-
-  $offset = 0 ;
-
-  foreach( $stanbolResponse['semantic'] as $key => $values ) {
-      $entity = $values['entities'][0] ;
-      $numSuggestions = count( $values['entities'] ) ;
-
-      $tag = "<a href=\"{$entity['uri']}\" class=\"xowl-suggestion\" data-cke-annotation=\"{$entity['label']}\" data-cke-type=\"{$entity['type']}\" data-cke-suggestions=\"{$numSuggestions}\">{$values['text']}</a>";
-
-
-      $start = $values['start'] + $offset ;
-      $end = $values['end'] + $offset ;
-
-      $length = mb_strlen($values['text'] ) ; 
-
-      $text = $this->subFromString( $text , $start, $length, $tag ) ;
-
-      $offset +=  ( mb_strlen( $tag) -  mb_strlen( $values['text']) )     ;
-
-  }
-//   print_R( $text ) ;
-  
-  $stanbolResponse['text'] = $text ;
-
-
-
-
-
-
-
-    header('Content-type: application/json');
-    die( json_encode( $stanbolResponse ) ) ;
- 
-  }
-     private function subFromString( $string, $start, $length, $sub = '' ) {
+    private function subFromString( $string, $start, $length, $sub = '' ) {
         $result =  mb_substr( $string , 0, $start ) . $sub  . mb_substr( $string , $start + $length ) ;
         return $result ;
     }
- 
+
 }
